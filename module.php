0b326b9d4132522944d52b0a4858da22b4e74420
Alexander Kuzmin <roosit@abricos.org>
2013-01-19 13:55:20 +0400

    Исправлен заголовок

--- module.php
+++ module.php
@@ -2,10 +2,8 @@
 /**
  * Модуль Widget
  * 
- * @version $Id: module.php 1030 2011-07-07 04:56:30Z roosit $
  * @package Abricos 
  * @subpackage Widget
- * @copyright Copyright (C) 2011 Abricos All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * @author Alexander Kuzmin (roosit@abricos.org)
  */
