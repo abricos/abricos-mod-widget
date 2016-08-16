6541adf6af8d5916aad37c91ae4eb51ff9612d6c
Alexander Kuzmin <roosit@abricos.org>
2016-08-16 18:27:05 +0300

    source formatting

--- src/module.php
+++ src/module.php
@@ -1,23 +1,24 @@
-<?php 
+<?php
 /**
  * Модуль Widget
- * 
- * @package Abricos 
+ *
+ * @package Abricos
  * @subpackage Widget
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * @author Alexander Kuzmin <roosit@abricos.org>
  */
 
+/**
+ * Class WidgetModule
+ */
 class WidgetModule extends Ab_Module {
-	public function __construct(){
-		// Версия модуля
-		$this->version = "0.1.4";
-		
-		// Название модуля
-		$this->name = "widget";
-	}
+    public function __construct(){
+        // Версия модуля
+        $this->version = "0.1.4";
+
+        // Название модуля
+        $this->name = "widget";
+    }
 }
 
 Abricos::ModuleRegister(new WidgetModule());
-
-?>
